c27c890b3defbcf3a3d95f4caaf721e47cd22617
Ana Mafla <anamafla@hotmail.com>
2018-08-24 13:31:13 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,9 @@
 # Write your #display_rainbow method here
 def display_rainbow(array)
  puts "#{array[0][0].upcase}: #{array[0]}, 
- #{array[1][0].}: #{array[1]}, 
- #{array[2][0]}: #{array[2]}, "
+ #{array[1][0].upcase}: #{array[1]}, 
+ #{array[2][0].upcase}: #{array[2]},
+ #{array[3][0].upcase}: #{array[3]},
+ #{array[4][0].upcase}: #{array[4]},
+ "
 end
