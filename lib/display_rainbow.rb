0a9327fc84d5cf4c2ab6585981f19309fb53859d
Ana Mafla <anamafla@hotmail.com>
2018-08-24 13:33:45 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -5,7 +5,8 @@ def display_rainbow(array)
  #{array[2][0].upcase}: #{array[2]},
  #{array[3][0].upcase}: #{array[3]},
  #{array[4][0].upcase}: #{array[4]},
- 
+ #{array[5][0].upcase}: #{array[5]},
+ #{array[6][0].upcase}: #{array[6]},
  
 "
 end
