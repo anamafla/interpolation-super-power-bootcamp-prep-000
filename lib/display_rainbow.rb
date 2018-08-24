77457e6c8caea121dd03ec50c0b291f6287f3347
Ana Mafla <anamafla@hotmail.com>
2018-08-24 13:29:46 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 # Write your #display_rainbow method here
 def display_rainbow(array)
- puts "#{array[0][0]}: #{array[0]}, 
+ puts "#{array[0][0]}.uppcase: #{array[0]}, 
  #{array[1][0]}: #{array[1]}, 
  #{array[2][0]}: #{array[2]}, "
 end
